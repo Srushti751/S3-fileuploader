2b30b1d5321d2673e0c6dde2c65d4babf63d4e6f
Srushti751 <srushti.shetty@neosoftmail.com>
2022-06-29 13:12:00 +0530

    upload using presigned url

--- fileupload/src/components/Fileupload.js
+++ fileupload/src/components/Fileupload.js
@@ -3,124 +3,102 @@ import axios from "axios";
 import { saveAs } from "file-saver";
 
 function Fileupload() {
-    const [uploadedFile, setUploadedFile] = useState(null);
-    const [files, setFiles] = useState([]);
+  const [uploadedFile, setUploadedFile] = useState(null);
+  const [files, setFiles] = useState([]);
 
-    const chooseFile = (event) => {
-        setUploadedFile(event.target.files[0]);
+  const chooseFile = (event) => {
+    setUploadedFile(event.target.files[0]);
+  };
+
+  //Upload using presigned url
+  const UploadHandler = async (e) => {
+    e.preventDefault();
+    const file = document.querySelector('input[name="file"]');
+    const resss = await axios.get("/get-signed-url", {
+      headers: { filename: file.files[0].name },
+    });
+    const url = resss.data.url;
+    const fields = resss.data.fields;
+    console.log(file.files[0].name);
+    const data = {
+      bucket: "assessmentbucket57",
+      ...fields,
+      "Content-Type": file.files[0].type,
+
+      file: file.files[0],
     };
 
-    //Upload using presigned url
-    // const UploadHandler = async (e) => {
-    //     e.preventDefault()
-    //     const file = document.querySelector('input[name="file"]');
-    //     const resss = await axios.get("/get-signed-url")
-    //     const url = resss.data.url
-    //     const fields = resss.data.fields
-    //     const data = {
-    //         bucket: "assessmentbucket57",
-    //         ...fields,
-    //         'Content-Type': file.files[0].type,
+    const formData = new FormData();
+    for (const name in data) {
+      console.log(data[name]);
+      formData.append(name, data[name]);
+    }
 
-    //         file: file.files[0]
-    //     };
-    //     const formData = new FormData();
-    //     for (const name in data) {
-    //         formData.append(name, data[name]);
-    //     }
+    await axios.post(url, formData);
+  };
 
-    //     var options = {
-    //         headers: { 'Content-type': file.files[0].type, 'x-amz-acl': 'public-read' },
+  //Function to fetch files
+  const getFiles = async () => {
+    axios
+      .get("/getfiles")
+      .then((res) => {
+        setFiles([res.data]);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    //     }
-    //     const response = await fetch(url, {
-    //         method: 'PUT',
-    //         body: formData,
-    //         options
-    //     })
-    //     console.log(response)
-    // }
+  //Function to download a particular file
+  const onDownload = (name) => {
+    axios
+      .get(`/download/${name}`, { responseType: "blob" })
+      .then((res) => {
+        var file = new Blob([res.data], { type: res.headers["content-type"] });
+        saveAs(file, `${name}`);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    // Function to upload files
-    const newUpdateHandler = async (e) => {
-        try {
-            e.preventDefault();
-            const file = document.querySelector('input[name="file"]');
+  useEffect(() => {
+    getFiles();
+  }, []);
 
-            //create object of form data
-            const formData = new FormData();
-            await formData.append("file", uploadedFile);
-            await axios.post("/uploadfile", formData);
-            alert("Upload successfull!");
-        }
-        catch (e) {
-            console.log("Cathc: ", e);
-        }
+  return (
+    <>
+      <form className="container" encType="multipart/form-data">
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Choose File to Upload
+          </label>
+          <input
+            type="file"
+            name="file"
+            onChange={chooseFile}
+            className="form-control"
+          />
+        </div>
+        <button
+          type="submit"
+          onClick={(e) => UploadHandler(e)}
+          className="btn btn-dark mb-5"
+        >
+          Upload
+        </button>
+      </form>
 
-    };
-
-    //Function to fetch files
-    const getFiles = async () => {
-        axios
-            .get("/getfiles")
-            .then((res) => {
-                setFiles([res.data]);
-            })
-            .catch((err) => console.log(err));
-    };
-
-    //Function to download a particular file
-    const onDownload = (name) => {
-        axios
-            .get(`/download/${name}`, { responseType: "blob" })
-            .then((res) => {
-                var file = new Blob([res.data], { type: res.headers["content-type"] });
-                saveAs(file, `${name}`);
-            })
-            .catch((err) => console.log(err));
-    };
-
-    useEffect(() => {
-        getFiles();
-    }, []);
-
-    return (
-        <>
-            <form className="container" encType="multipart/form-data">
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">
-                        Choose File to Upload
-                    </label>
-                    <input
-                        type="file"
-                        name="file"
-                        onChange={chooseFile}
-                        className="form-control"
-                    />
-                </div>
-                <button
-                    type="submit"
-                    onClick={(e) => newUpdateHandler(e)}
-                    className="btn btn-dark mb-5"
-                >
-                    Upload
-                </button>
-            </form>
-
-            <div className="list-group container ">
-                {files[0] &&
-                    files[0].map((file, index) => {
-                        return (
-                            <button onClick={(e) => onDownload(file)} className="mb-2">
-                                <li className="list-group-item " key={index}>
-                                    {file}
-                                </li>
-                            </button>
-                        );
-                    })}
-            </div>
-        </>
-    );
+      <div className="list-group container ">
+        {files[0] &&
+          files[0].map((file, index) => {
+            return (
+              <button onClick={(e) => onDownload(file)} className="mb-2">
+                <li className="list-group-item " key={index}>
+                  {file}
+                </li>
+              </button>
+            );
+          })}
+      </div>
+    </>
+  );
 }
 
 export default Fileupload;
--- uploadserver/index.js
+++ uploadserver/index.js
@@ -1,75 +1,59 @@
-require("dotenv").config()
-const express = require('express')
-const cors = require('cors')
-const fileupload = require('express-fileupload')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const fileupload = require("express-fileupload");
 
 const app = express();
 
-app.use(cors())
-app.use(fileupload())
-
+app.use(cors());
+app.use(fileupload());
 
 app.listen(3001, (req, res) => {
-    console.log("server running")
+  console.log("server running");
 });
 
-const aws = require('aws-sdk')
-
+const aws = require("aws-sdk");
 
 // AWS IAM user configuration
 aws.config.update({
-    secretAccessKey: process.env.ACCESS_SECRET,
-    accessKeyId: process.env.ACCESS_KEY,
-    region: process.env.REGION,
-
+  secretAccessKey: process.env.ACCESS_SECRET,
+  accessKeyId: process.env.ACCESS_KEY,
+  region: process.env.REGION,
 });
-const BUCKET = process.env.BUCKET
+const BUCKET = process.env.BUCKET;
 const s3 = new aws.S3();
 
-// API to get signed url 
-// app.get('/get-signed-url', async (req, res) => {
-//     await s3.createPresignedPost({
-//         Fields: {
-//             key: uuidv4(),
-//         },
-//         headers: {
-//             'Access-Control-Allow-Origin': '*',
-//             'Access-Control-Allow-Credentials': true
-//         },
-//         Expires: 600,
-//         Bucket: BUCKET,
-//     }, (err, signed) => {
-//         res.json(signed);
-//     });
-// })
-
-// API for uploading files to Private S3 Bucket
-app.post("/uploadfile", async function (req, res, next) {
-    let upload_params = { Bucket: BUCKET, Key: req.files.file.name, Body: req.files.file.data };
-    let upload = new aws.S3.ManagedUpload({ params: upload_params });
-    let promise = upload.promise();
-    promise.then(
-        function (data) { console.log("Successfully uploaded:", "file"); res.send("Upload successful") },
-        function (err) { console.log("Failed to upload", "with error:", err.message); res.send("Error") },
-
-    )
-        .catch(e => res.send("Error"))
-
-})
+// API to get signed url
+app.get("/get-signed-url", async (req, res) => {
+  await s3.createPresignedPost(
+    {
+      Fields: {
+        key: req.headers.filename,
+      },
+      Conditions: [
+        ["starts-with", "$Content-Type", "image/"],
+        ["content-length-range", 0, 1000000],
+      ],
+      Expires: 600,
+      Bucket: BUCKET,
+    },
+    (err, signed) => {
+      res.json(signed);
+    }
+  );
+});
 
 // API for fetching files from  S3 Bucket
 
 app.get("/getfiles", async (req, res) => {
-
-    let r = await s3.listObjectsV2({ Bucket: BUCKET }).promise();
-    let x = r.Contents.map(item => item.Key);
-    res.send(x)
-})
+  let r = await s3.listObjectsV2({ Bucket: BUCKET }).promise();
+  let x = r.Contents.map((item) => item.Key);
+  res.send(x);
+});
 
 // API to download the file
 app.get("/download/:filename", async (req, res) => {
-    const filename = req.params.filename
-    let x = await s3.getObject({ Bucket: BUCKET, Key: filename }).promise();
-    res.send(x.Body)
-})
-
+  const filename = req.params.filename;
+  let x = await s3.getObject({ Bucket: BUCKET, Key: filename }).promise();
+  res.send(x.Body);
+});
